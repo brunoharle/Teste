53c8e2b1d63f22874e7af14f6f0ddb99d3554a96
AntonioFalcao <antonio_contato@live.com>
2017-07-22 16:20:52 -0300

    Painel, senha e guichê.

--- Git/src/git/Git.java
+++ Git/src/git/Git.java
@@ -1,11 +1,15 @@
-
 package git;
 
 public class Git {
 
     public static void main(String[] args) {
-       
+
         System.out.println("Lucas bixona!");
+        System.out.println("Lucas bixona!");
+        System.out.println("Lucas bixona!");
+        System.out.println("Lucas bixona!");
+        System.out.println("Lucas bixona!");
+
     }
-    
+
 }
